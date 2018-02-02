12503dd10efba8dc598ad2f43f13daf838e2e88f
Volha Kaleda <volhakaleda@ac.c4q.nyc>
2018-02-02 01:14:47 -0500

    RV working, DB created, image is not downloading

---
+++ app/src/main/java/backend/FellowDao.java
@@ -0,0 +1,33 @@
+package backend;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.babimaji.stayintouch.model.Fellow;
+
+import java.util.List;
+
+@Dao
+public interface FellowDao {
+
+    @Query("SELECT * FROM Fellow")
+    List<Fellow> getFellow();
+
+    @Query("SELECT * FROM Fellow WHERE id =:id")
+    Fellow getFellow(int id);
+
+    @Insert
+    void addAll(Fellow ...fellows);
+
+    @Insert
+    void insertFellow(Fellow fellow);
+
+    @Update
+    int updateFellow(Fellow fellow);
+
+    @Delete
+    int deleteFellow(Fellow fellow);
+}
